db11573295dbf17c1b535bf638308258f0c6f3c4
Maximilian Knoll <maximilian.knoll@gmail.com>
2018-06-09 16:19:45 +0200

    upd

--- R/cantEnc.R
+++ R/cantEnc.R
@@ -1,17 +1,22 @@
 #' @title Cantor Encode
 #' @export
-encCant <- function(text) {
+encCant <- function(text, off=32, maxLen=25) {
     words <- unlist(strsplit(text, " "))
     out <- c()
 
     ## TODO: forecah
+    rm <- c()
+    wrd <- c()
     for (word in words) {
         wordR <- rev(strsplit(word, "|")[[1]])
-        vals <- sapply(wordR, utf8ToInt)-32
+        vals <- sapply(wordR, utf8ToInt)-off
+        #max length of words
+        if (length(vals) > maxLen) { next )
         out <- c(out, cant(vals))
+        wrd <- c(wrd, word)
     }
 
-    names(out) <- words
+    names(out) <- wrd
     return(out)
 }
 
@@ -22,7 +27,7 @@ encCant <- function(text) {
 cant <- function(x) {
     if (length(x) > 2) {
         tmp <- cant(x[-length(x)])
-        print(paste("   ", tmp, " len: ", length(x), " x=", paste(x, collapse="|")))
+        #print(paste("   ", tmp, " len: ", length(x), " x=", paste(x, collapse="|")))
         return (cant(c(tmp, x[length(x)])))
     } else {
         x0 <- x[1]
