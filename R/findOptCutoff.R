ee88a8238c05d0ea6fab5cf5280efa486770967a
Maximilian Knoll <maximilian.knoll@gmail.com>
2018-08-20 12:41:54 +0200

    added cluster / subject

--- R/findOptCutoff.R
+++ R/findOptCutoff.R
@@ -8,6 +8,8 @@
 #' @param delta proportion in which group splitting should 
 #' be performed 
 #' @param minGrpSize minimum group size for cutoffs
+#' @param subject subject IDs if multiple measurements per subjects 
+#' are to be tested
 #' 
 #' @import foreach
 #' @import doParallel
@@ -19,11 +21,12 @@
 #' @return data.frame with the calculated survival values 
 #' (p.value, median survival, group sizes, cutoffsize, 
 #' stepsize)
-findOptCutoff <- function(data, srv, delta=0.1, minGrpSize=1) {
+findOptCutoff <- function(data, srv, delta=0.1, minGrpSize=1, subject=NULL) {
     ##remove NA SRV data 
     keep <- which(!is.na(srv))
     srv <- srv[keep]
     data <- data[keep]
+    subject <- subject[keep]
     
     cutoff <- max(data)
     step = abs(max(data, na.rm=T)-min(data, na.rm=T))*delta
@@ -48,32 +51,57 @@ findOptCutoff <- function(data, srv, delta=0.1, minGrpSize=1) {
         #print(cutoff)
         grp <- ifelse(data > cutoff, "high", "low")
         cutoff <- cutoff-step
-        if (length(levels(factor(grp))) == 2 ) {
-            fit <- survfit(srv~grp)
-            sdf <- survdiff(srv~grp)
-            p.val <- 1 - pchisq(sdf$chisq, length(sdf$n) - 1)
-            
-            ##univariate coxph
-            grp <- factor(grp)
-            grp <- relevel(grp, "low")
-            fit2 <- coxph(srv~factor(grp))
-            
-            data.frame(cutoff=cutoff+step, 
-                       p.val=p.val,
-                       nHigh=length(grp[grp == "high"]), 
-                       nLow=length(grp[grp == "low"]), 
-                       medianHigh=summary(fit)$table["grp=high","median"],
-                       medianLow=summary(fit)$table["grp=low","median"],
-                       delta=delta,
-                       coxph_name=rownames(summary(fit2)$conf.int)[1],
-                       coxph_hr=summary(fit2)$conf.int[1,1],
-                       coxph_lower=summary(fit2)$conf.int[1,3],
-                       coxph_upper=summary(fit2)$conf.int[1,4],
-                       coxph_p=summary(fit2)$coeff[1,5],
-                       coxph_lrt=summary(fit2)$logtest[[3]],
-                       coxph_score=summary(fit2)$sctest[[3]],
-                       coxph_wald=summary(fit2)$waldtest[[3]])
-        }
+        if (length(levels(factor(grp))) == 2) {
+	    if (is.null(subject)) {
+		fit <- survfit(srv~grp)
+		sdf <- survdiff(srv~grp)
+		p.val <- 1 - pchisq(sdf$chisq, length(sdf$n) - 1)
+
+		##univariate coxph
+		grp <- factor(grp)
+		grp <- relevel(grp, "low")
+		fit2 <- coxph(srv~factor(grp))
+
+		data.frame(cutoff=cutoff+step, 
+			   p.val=p.val,
+			   nHigh=length(grp[grp == "high"]), 
+			   nLow=length(grp[grp == "low"]), 
+			   medianHigh=summary(fit)$table["grp=high","median"],
+			   medianLow=summary(fit)$table["grp=low","median"],
+			   delta=delta,
+			   coxph_name=rownames(summary(fit2)$conf.int)[1],
+			   coxph_hr=summary(fit2)$conf.int[1,1],
+			   coxph_lower=summary(fit2)$conf.int[1,3],
+			   coxph_upper=summary(fit2)$conf.int[1,4],
+			   coxph_p=summary(fit2)$coeff[1,5],
+			   coxph_lrt=summary(fit2)$logtest[[3]],
+			   coxph_score=summary(fit2)$sctest[[3]],
+			   coxph_wald=summary(fit2)$waldtest[[3]])
+	    } else {
+		## paired data
+		# only coxph
+		grp <- factor(grp)
+		grp <- relevel(grp, "low")
+		fit2 <- coxph(srv~factor(grp)+cluster(subject))
+		p.val <- summary(fit2)$logtest[[3]]
+
+		data.frame(cutoff=cutoff+step, 
+			   p.val=p.val,
+			   nHigh=length(grp[grp == "high"]), 
+			   nLow=length(grp[grp == "low"]), 
+			   medianHigh=NA, #summary(fit)$table["grp=high","median"],
+			   medianLow=NA, #summary(fit)$table["grp=low","median"],
+			   delta=delta,
+			   coxph_name=rownames(summary(fit2)$conf.int)[1],
+			   coxph_hr=summary(fit2)$conf.int[1,1],
+			   coxph_lower=summary(fit2)$conf.int[1,3],
+			   coxph_upper=summary(fit2)$conf.int[1,4],
+			   coxph_p=summary(fit2)$coeff[1,5],
+			   coxph_lrt=summary(fit2)$logtest[[3]],
+			   coxph_score=summary(fit2)$sctest[[3]],
+			   coxph_wald=summary(fit2)$waldtest[[3]])
+	    }
+	}
     }
     coll <- do.call(rbind, out)
     coll <- coll[order(coll$p.val),]
