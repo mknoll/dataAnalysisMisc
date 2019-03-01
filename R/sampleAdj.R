c608de1b2c0d2823deb9badfc64f117bb76e7a90
Maximilian Knoll <maximilian.knoll@gmail.com>
2019-03-01 19:58:21 +0100

    specify number of nodes or number of edged

--- R/sampleAdj.R
+++ R/sampleAdj.R
@@ -7,19 +7,23 @@
 #' @param n Number of clusters
 #' @param maxTry numbers of random tries to establish clusters
 #' @param starTopo create star topology
+#' @param type If set to 0, n refers to number of edges, choose
+#' 1 for number of nodes
 #'
 #' @useDynLib dataAnalysisMisc
 #'
 #' @export 
 sampleAdj <- function(adj, size=5, 
 		      n=5, maxTry=1000,
-		      starTopo=1) {
+		      starTopo=1,
+              type=1) {
 
     m <- c(as.matrix(unlist(adj), nrow=1))
     erg <- c(as.integer(rep(0, length(m))))
     ret <- .C("startAdj", matrix=as.integer(m), len=length(adj[1,]), 
 	      n=as.integer(n), size=as.integer(size), erg=erg, 
-	      maxTry=as.integer(maxTry), star=as.integer(starTopo))
+	      maxTry=as.integer(maxTry), star=as.integer(starTopo),
+          type=as.integer(type))
     tbl <- table(ret$erg)
     if (length(tbl)-1 != n) { 
 	warning("To few clusters identified! Try increasing the maxTry parameter.")
--- src/graph.c
+++ src/graph.c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-int findeNachbar(int *m, int *visited, int start, int invStart, int len,  int size, int found, int clID, int star);
-int findCluster(int *m, int *visited, int len, int size, int id, int star);
+int findeNachbar(int *m, int *visited, int start, int invStart, int len,  int size, int found, int clID, int star, int type);
+int findCluster(int *m, int *visited, int len, int size, int id, int star, int type);
+int countClusterMembers(int *visited, int clID, int size, int len);
 
-void startAdj(int *matrix, int *len, int *n, int *size, int *erg, int *maxTry, int *star) {
+void startAdj(int *matrix, int *len, int *n, int *size, int *erg, int *maxTry, int *star, int *type) {
     printf("Matrix: %d x %d \n", len[0], len[0]);
     printf("Pos [0,0]: %d\n", matrix[0]);
 
@@ -44,7 +45,7 @@ void startAdj(int *matrix, int *len, int *n, int *size, int *erg, int *maxTry, i
 	int success;
         do {
             versuche--;
-	    success = findCluster(m, visited, len[0], size[0], i, star[0]);
+	    success = findCluster(m, visited, len[0], size[0], i, star[0], type[0]);
         } while (!success && versuche > 0);
 	//printf("SUCCESS CLUSTER: %d\n", success);
     }
@@ -58,7 +59,7 @@ void startAdj(int *matrix, int *len, int *n, int *size, int *erg, int *maxTry, i
 //Findet zusammenhängede Cluster der Größe size
 //Suche in adj, teste, ob in visited schon besucht,
 //markiere als besucht in visited, return bei Erfolg
-int findCluster(int *m, int *visited, int len, int size, int id, int star) {
+int findCluster(int *m, int *visited, int len, int size, int id, int star, int type) {
     //Finde zufälligen startpunkt
     int maxIndex = len*len;
     int start = rand() % (maxIndex + 1);
@@ -86,7 +87,7 @@ int findCluster(int *m, int *visited, int len, int size, int id, int star) {
     tmpVisited[invStart] = id;
 
     // Finde nachbar
-    int success = findeNachbar(m, tmpVisited, start, invStart, len, size, 1, id, star);
+    int success = findeNachbar(m, tmpVisited, start, invStart, len, size, 1, id, star, type);
     if (success == 1) {
 	printf("Copy back\n");
 	memcpy(visited, tmpVisited, maxIndex*sizeof(tmpVisited));
@@ -111,7 +112,7 @@ int findCluster(int *m, int *visited, int len, int size, int id, int star) {
 }
 
 
-int findeNachbar(int *m, int *visited, int start, int invStart, int len, int size, int found, int clID, int star) {
+int findeNachbar(int *m, int *visited, int start, int invStart, int len, int size, int found, int clID, int star, int type) {
     //printf("SIZE %d, FOUND %d\n", size, found);
     if (size == found) { 
 	return(1);
@@ -178,9 +179,88 @@ int findeNachbar(int *m, int *visited, int start, int invStart, int len, int siz
 	visited[invNachbar] = clID;
 	printf("\tREGISTER: [%d, %d], [%d, %d]\n", nachbar%len, nachbar / len, invNachbar%len, invNachbar / len);
 
-	found++;
-	//printf("Currently found: %d\n", found);
-	return(findeNachbar(m, visited, nachbar, invNachbar, len, size, found, clID, star));
+    int counted = countClusterMembers(visited, clID, size, len);
+    printf("DIFF: %d\n", counted-found);
+    if (type == 1) {
+        //Number of nodes
+        if (counted-found >= 1 ) {
+            found++;
+        }
+    } else {
+        //Number of edged
+	    found++;
+    }
+
+	printf("Currently found: %d; counted: %d \n", found, counted);
+
+
+	return(findeNachbar(m, visited, nachbar, invNachbar, len, size, found, clID, star, type));
     } 
     return(0);
+}
+
+int countClusterMembers(int *visited, int clID, int size, int len) {
+    // all visited
+    int *pos = malloc(size*sizeof(pos));
+    for (int i=0; i < size; i++) {
+        pos[i] = -1;
+    }
+
+    for (int i=0; i < len*len; i++) {
+        if (visited[i] == clID) {
+            //printf("FOUND MEMBER: ");
+            //Found a cluster member
+            int iInv = len*(i%len)+i/len;
+            int found, index;
+            int c; 
+            c = i/len;
+            //printf(" %d ", c);
+            //
+            found = 0;
+            index = 0;
+            for (int z=0; z < size; z++) {
+                if (pos[z] == c) {
+                    found = 1;
+                    break;
+                }
+                if (pos[z] == -1) {
+                    index = z;
+                }
+            }
+            if (found == 0) {
+                pos[index] = c;
+            }
+            ///
+            c = i%len;
+            //printf(" %d \n", c);
+            found = 0;
+            index = 0;
+            for (int z=0; z < size; z++) {
+                if (pos[z] == c) {
+                    found = 1;
+                    break;
+                }
+                if (pos[z] == -1) {
+                    index = z;
+                }
+            }
+            if (found == 0) {
+                pos[index] = c;
+            }
+        }
+    }
+            
+    int count =0;
+    //printf("POS: ");
+    for (int z=0; z < size; z++) {
+        //printf("%d ", pos[z]);
+        if (pos[z] != -1) {
+            count++;
+        }
+    }
+    //printf("\n");
+
+    free(pos);
+
+    return(count);
 }
